87ce9aae75f5f4e98f3a1396d975cdf40c1ab3b4
sebagm5 <seba_gm@hotmail.com>
2021-11-15 12:55:04 -0300

    3rd commit

--- DemoProjectgit/src/GitTest.java
+++ DemoProjectgit/src/GitTest.java
@@ -3,7 +3,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("push");
 		System.out.println("new comment");
 	}
 
